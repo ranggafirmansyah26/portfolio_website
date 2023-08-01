c061f0169c8eb87b507fdd7d7d1331603353f3e6
Rangga Firmansyah <ranggafirmansyah272@gmail.com>
2023-08-01 10:19:11 +0000

    Update Hero.jsx
    Add "Contact Me" button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import { BsLinkedin, BsGithub } from "react-icons/bs";
 
 export default function Hero(){
 	return(
-		<div className="flex flex-col h-screen items-center justify-center text-white">
+		<div className="flex flex-col h-screen items-center justify-center text-white relative">
 			<h1 className="text-5xl font-bold mb-2 drop-shadow-lg"><span className="text-7xl">R</span>angga Firmansyah</h1>
 			<h2 className="text-2xl font-bold text-[#FCC642]">Frontend Developer</h2>
 			<div className="flex gap-4 mt-4">
@@ -11,6 +11,11 @@ export default function Hero(){
 				</a>
 				<a href="">
 					<BsGithub size="32"/>
+				</a>
+			</div>
+			<div className="absolute bottom-[17%]">
+				<a href="" className="flex items-center justify-center bg-gradient-to-br from-[#6bd0ff] to-sky-500 w-56 py-4 text-[18px] text-[#ffffff] rounded-md font-bold block">
+					<span className="drop-shadow">Contact Me</span>
 				</a>
 			</div>
 		</div>
